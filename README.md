525d7b78f16e491ae57fce348f30a739fcfa2e7b
ErvinGU <ervin.garay+1@gmail.com>
2025-04-25 02:00:09 +0000

    jquery

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # jquery_ejemplo
 practica jquery
+
+
+    <p class="text-center">este es un parrafo</p>
+
+    <div id="div">Este es un div</div>
+
+    <button id="boton">click aquí</button>
+ 
+ js
+ $("p").click(function(){
+    $(this).hide(4000)
+    $(this).fadeIn(4000)
+
+})
+
+$("#boton").click(function(){
+    $("#div").css({
+        'background-color': 'chocolate',
+        'color': '#fff',
+        'padding': '10px'
+    })
+    
+})
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,17 +3,36 @@
 
 
 
-$("p").click(function(){
-    $(this).hide(4000)
-    $(this).fadeIn(4000)
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+
+$("#btn").click(function(){
+    alert("se toco el boton")
+})
+
+$("#btn2").dblclick(function(){
+    alert("se toco el boton 2 veces")
+})
+
+$(".hover").hover(
+    function() {$(this).css("background-color", "yellow")},
+    function() {$(this).css("background-color", "")},
+
+)
+
+$("h2").dblclick(function(){
+    $(this).css("color", "red")
 
 })
 
-$("#boton").click(function(){
-    $("#div").css({
-        'background-color': 'chocolate',
-        'color': '#fff',
-        'padding': '10px'
-    })
-    
+$("#btntoggle").click(function(){
+    $("#contenido").toggle()
 })
